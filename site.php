f88e0237c0fdd6464e61cab993fa8242cab6827f
Bineeth B C <bineethbcr7007@gmail.com>
2023-08-06 13:13:32 +0530

    function

---
+++ site.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+ <!-- Functions -->
+ <!-- contianer, bunch of code designed to perform a specific task -->
+    <?php  function sayHi(){
+        echo "Hello user";
+        }
+        sayHi();
+    ?>
+
+    </body>
+    
+</html>
