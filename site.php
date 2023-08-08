48b962cd08f685953fffe8ff1179e0c0e6e7d693
Bineeth B C <bineethbcr7007@gmail.com>
2023-08-08 13:09:16 +0530

    Class & Objects

--- site.php
+++ site.php
@@ -10,11 +10,27 @@
         <link rel="stylesheet" href="">
     </head>
     <body> 
-        <!-- include php -->
+        <!-- Classes & Objects -->
+        <!-- class Classname { } -->
+        <!-- objectname = new Classname -->
         <?php
-        include "useful-tools.php";
-        sayHi("Bineeth");
-        echo $feetInMile;
+            class Book {   //class: blueprint of objects 
+                 var $title;
+                 var $author;
+                 var $pages;
+            }
+            $book1 = new Book;  //object: instance of a class
+            $book1->title = "Harry Potter";
+            $book1->author = "JK Rowling";
+            $book1->pages = 400;
+
+            $book2 = new Book;  //object: instance of a class
+            $book2->title = "Lord of the Rings";
+            $book2->author = "Tolkien";
+            $book2->pages = 700;
+            
+            echo $book1->title;
+            echo $book2->title;
         ?>
 
     </body>
