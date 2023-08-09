7f1b04381ce0ab6c11a9b2be8e4576601e6dcc9d
Bineeth B C <bineethbcr7007@gmail.com>
2023-08-09 19:46:36 +0530

    constructor begin

--- site.php
+++ site.php
@@ -10,8 +10,36 @@
         <link rel="stylesheet" href="">
     </head>
     <body> 
-        <!-- conflicts -->
+        <!-- constructors -->
+        <!-- basically a fuction get call
+        whenever we create an object
+        of the class -->
+        <!-- function __construct(){
+                    
+                } -->
+        <?php
+        class Book {   //class: blueprint of objects 
+                 var $title;
+                 var $author;
+                 var $pages;
 
+                 function __construct($name){  //constructor
+                    echo "$name<br>";
+                 }
+            }
+            $book1 = new Book("Bineeth");  //object: instance of a class
+            $book1->title = "Harry Potter";
+            $book1->author = "JK Rowling";
+            $book1->pages = 400;
+
+            $book2 = new Book("Jinshu");  //object: instance of a class
+            $book2->title = "Lord of the Rings";
+            $book2->author = "Tolkien";
+            $book2->pages = 700;  
+
+            echo $book1->title;
+            echo $book2->title;
+        ?>
     </body>
     
 </html>
